e03b3cfb832b5fdaaa775e6bfefbf0b907087a78
Andrre Lima <andre.salatalima@gmail.com>
2022-05-28 21:01:11 +0200

    correção do view do forme e importação

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,2 +1,6 @@
 class FormsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_003058) do
+ActiveRecord::Schema.define(version: 2022_05_28_151528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,17 @@ ActiveRecord::Schema.define(version: 2022_05_27_003058) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_answers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.bigint "answer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_id"], name: "index_user_answers_on_answer_id"
+    t.index ["question_id"], name: "index_user_answers_on_question_id"
+    t.index ["user_id"], name: "index_user_answers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +93,7 @@ ActiveRecord::Schema.define(version: 2022_05_27_003058) do
   add_foreign_key "matches", "users"
   add_foreign_key "therapist_specialties", "specialties"
   add_foreign_key "therapist_specialties", "therapists"
+  add_foreign_key "user_answers", "answers"
+  add_foreign_key "user_answers", "questions"
+  add_foreign_key "user_answers", "users"
 end
