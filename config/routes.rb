4eb6f97e177af1b947ed7c13b92b7bf062a58dbe
Andrre Lima <andre.salatalima@gmail.com>
2022-05-28 23:20:03 +0200

    included questions_controller, question_index, added routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get 'form', to: 'forms#show'
+  
+  get 'questions', to: 'questions#index', namespace: 'form'
   get 'therapist', to: 'therapists#show'
 
 
