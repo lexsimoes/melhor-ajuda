8a5873c405ea5dcee422414fcd40b287e1a4c0e9
davidsccp90 <davidsccp90@gmail.com>
2022-05-31 20:45:33 -0300

    created therapists seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Therapist.create( name: 'Peter Parker', age: 25, gender: 'Male', bio: 'Hello, My name is Peter Parker, and I am a Therapist with specialty in dealing with Depression' )
+Therapist.create( name: 'Mary Watson', age: 26, gender: 'Female', bio: 'Hello, My name is Mary Watson and I am a Therapist with specialty in dealing with Anxiety' )
+Therapist.create( name: 'Eddie Brock', age: 31, gender: 'Male', bio: 'Hello, My name is Eddie Brock and I am a Therapist with specialty in dealing with Addictions' )
+Therapist.create( name: 'Harry Osborn', age: 40, gender: 'Male', bio: 'Hello, My name is Harry Osborn, and I am a Therapist with specialty in dealing with Sexuality' )
+Therapist.create( name: 'Gwen Stacy', age: 24, gender: 'Female', bio: 'Hello, My name is Gwen Stacy and I am a Therapist with specialty in dealing with Relationships' )
