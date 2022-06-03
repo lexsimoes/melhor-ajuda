c2c5076f077bbc839634cc200e032c100163b603
davidsccp90 <davidsccp90@gmail.com>
2022-06-02 22:13:46 -0300

    therapist_show

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,2 +1,8 @@
 class TherapistsController < ApplicationController
+skip_before_action :authenticate_user!
+
+def show
+@therapist = Therapist.find(params[:id])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   get 'questions', to: 'questions#index', namespace: 'form'
-  get 'therapist', to: 'therapists#show'
+  get 'therapists/:id', to: 'therapists#show'
 
 
 end
