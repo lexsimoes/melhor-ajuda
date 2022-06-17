e74a257a29d215669e3dde658a5c3fa3f7c6ac9f
Andrre Lima <andre.salatalima@gmail.com>
2022-06-17 02:48:43 +0200

    whatsappp message included

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,24 +1,23 @@
 class TherapistsController < ApplicationController
 
-skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
-def show
-  @therapist = Therapist.find(params[:id])
-  @day_date = User.last.date.day
-  @month_date = User.last.date.month
-  @year_date = User.last.date.year
-  @time =  User.last.time.hour
-  @second_time_option =  User.last.second_time_option.hour
+  def show
+    @therapist = Therapist.find(params[:id])
+    @day_date = User.last.date.day
+    @month_date = User.last.date.month
+    @year_date = User.last.date.year
+    @time =  User.last.time
+    @second_time_option =  User.last.second_time_option.hour
 
-  @timepicker = [@time, @second_time_option].sample
+    @timepicker = [@time, @second_time_option].first
+  end
 
-end
+  def index
+  end
 
-def index
-end
-
-def user_answer_params
-  params.require(:user_answer).permit(:date, :time, :second_time_option, :email, :id)
-end
+  def user_answer_params
+    params.require(:user_answer).permit(:date, :time, :second_time_option, :email, :id)
+  end
 
 end
