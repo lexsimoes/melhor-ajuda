ce56bc983f0c82ca51875ce50d0db005b73928a7
Andrre Lima <andre.salatalima@gmail.com>
2022-06-17 04:42:43 +0200

    migration

--- db/migrate/20220617024046_add_second_time_to_user_answers.rb
+++ db/migrate/20220617024046_add_second_time_to_user_answers.rb
@@ -1,4 +1,4 @@
-class AddSecondTimeOptionToUserAnswers < ActiveRecord::Migration[6.1]
+class AddSecondTimeToUserAnswers < ActiveRecord::Migration[6.1]
   def change
     add_column :user_answers, :second_time_option, :string
   end
